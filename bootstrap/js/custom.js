997bbbb8d25c63e1137bcc45ae686a07a89c1246
Shwe Phue Hmone <109261829+ShwePhueHmone@users.noreply.github.com>
2022-12-28 10:41:00 +0630

    Delete bootstrap directory

--- bootstrap/js/custom.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-  $(document).scroll(function () {
-    var $nav = $(".navbar");
-    var $btn = $(".get-btn");
-    $btn.toggleClass("scrolled", $(this).scrollTop() > $btn.height());
-    $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
-  });
-});
